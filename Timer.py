350b78265c4ab142e54319cd76e3db0142327c3b
Jose Penalver Bartolome <penalverbj@gmail.com>
2021-05-18 00:58:26 +0000

    code formating

--- Timer.py
+++ Timer.py
@@ -1,17 +1,15 @@
 #timer class will house all the functionality that has to do with implementing timers
 import time
 
+
 class Timer:
-  #static variable that will house all the current timers
-  #will be used when we want to implement the deleting timer functionality
-  timers = {}
+    #static variable that will house all the current timers
+    #will be used when we want to implement the deleting timer functionality
+    timers = {}
 
-  #t = time in seconds that serves as value
-  #name = name string that serves as key    
-  def __init__(self, name, t):
-    self.name = name
-    self.t = t
-    Timer.timers.update({name : t})
-    
-
-
+    #t = time in seconds that serves as value
+    #name = name string that serves as key
+    def __init__(self, name, t):
+        self.name = name
+        self.t = t
+        Timer.timers.update({name: t})
--- main.py
+++ main.py
@@ -11,71 +11,73 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  msg = message.content
-  channel = message.channel
-  if message.author == client.user:
-    return
+    msg = message.content
+    channel = message.channel
+    if message.author == client.user:
+        return
 
-	#TIMERS
-	#command is $at. 
-	#a-Alfonso. t-timer. ' ' - to separate it from time constant
-  elif msg.startswith('$at '):
-    x = msg.split(" ")
-    name = x[1]
-    print(name)
-    inTime = x[2].split(":")
-    hour = inTime[0]
-    minute = inTime[1]
-    second = inTime[2]
-    secs = (hour * 60 * 60) + (minute * 60) + second
-    timer1 = Timer(name, secs)
-    print(Timer.timers)
-    print(timer1)
-    await channel.send("name: " + name + " hour: " +hour + " minute: " + minute + " second: " + second)
+#TIMERS
+#command is $at.
+#a-Alfonso. t-timer. ' ' - to separate it from time constant
+    elif msg.startswith('$at '):
+        x = msg.split(" ")
+        name = x[1]
+        print(name)
+        inTime = x[2].split(":")
+        hour = inTime[0]
+        minute = inTime[1]
+        second = inTime[2]
+        secs = (hour * 60 * 60) + (minute * 60) + second
+        timer1 = Timer(name, secs)
+        print(Timer.timers)
+        print(timer1)
+        await channel.send("name: " + name + " hour: " + hour + " minute: " +
+                           minute + " second: " + second)
 
-	#commad is $ast 
-	#s = seconds
-  elif msg.startswith('$ast '):
-    x = msg.split(" ")
-    name = x[1]
-    t = x[2]
-    timer1 = Timer(name, t)
-    print(Timer.timers)
-    print(timer1)
-    await channel.send("timer " + name + " set for " + t + " seconds")
-  
-  #commad is $amt 
-	#m = minutes
-  elif msg.startswith('$amt '):
-    x = msg.split(" ")
-    name = x[1]
-    t = x[2]
-    secs = t * 60
-    timer1 = Timer(name, secs)
-    print(Timer.timers)
-    print(timer1)
-    await channel.send("timer " + name + " set for " + t + " minutes")
+#commad is $ast
+#s = seconds
+    elif msg.startswith('$ast '):
+        x = msg.split(" ")
+        name = x[1]
+        t = x[2]
+        timer1 = Timer(name, t)
+        print(Timer.timers)
+        print(timer1)
+        await channel.send("timer " + name + " set for " + t + " seconds")
 
-  #commad is $aht 
-	#h = hours
-  elif msg.startswith('$aht '):
-    x = msg.split(" ")
-    name = x[1]
-    t = x[2]
-    secs = t * 60 * 60
-    timer1 = Timer(name, secs)
-    print(Timer.timers)
-    print(timer1)
-    await channel.send("timer " + name + " set for " + t + " hours")
+    #commad is $amt
+#m = minutes
+    elif msg.startswith('$amt '):
+        x = msg.split(" ")
+        name = x[1]
+        t = x[2]
+        secs = t * 60
+        timer1 = Timer(name, secs)
+        print(Timer.timers)
+        print(timer1)
+        await channel.send("timer " + name + " set for " + t + " minutes")
 
-  #instruction commands
-  elif msg.startswith('$ahelp'):
-	  await channel.send(constants.instructions)
-  elif msg.startswith('$atimers'):
-    await channel.send(constants.timers)
-  elif msg.startswith('$areminders'):
-    await channel.send(constants.reminders)
-  elif msg.startswith('$apolls'):
-    await channel.send(constants.polls)
+    #commad is $aht
+
+#h = hours
+    elif msg.startswith('$aht '):
+        x = msg.split(" ")
+        name = x[1]
+        t = x[2]
+        secs = t * 60 * 60
+        timer1 = Timer(name, secs)
+        print(Timer.timers)
+        print(timer1)
+        await channel.send("timer " + name + " set for " + t + " hours")
+
+    #instruction commands
+    elif msg.startswith('$ahelp'):
+        await channel.send(constants.instructions)
+    elif msg.startswith('$atimers'):
+        await channel.send(constants.timers)
+    elif msg.startswith('$areminders'):
+        await channel.send(constants.reminders)
+    elif msg.startswith('$apolls'):
+        await channel.send(constants.polls)
 
 client.run(os.environ['TOKEN'])
